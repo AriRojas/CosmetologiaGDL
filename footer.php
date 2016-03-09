1fd921606513f71998262e488195da65c58906d3
AriRojas <a7xariadna@gmail.com>
2016-03-08 22:10:05 -0600

    Cambios de tabulación en el footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
-	<footer>
+    <footer>
         <div class="row">
-		    <div class="col-lg-12">
-		    	<span class="text-footer">Zacatecas Norte #118, Unión de Tula, Jalisco.</span><br/>
-		    	<span class="text-footer">Tel. (316) 371-000</span></br>
-		        <span class="text-footer">Copyright &copy; Cosmetología Guadalajara</span>
-		    </div>
-		</div>
+            <div class="col-lg-12">
+                <span class="text-footer">Zacatecas Norte #118, Unión de Tula, Jalisco.</span><br/>
+                <span class="text-footer">Tel. (316) 371-000</span></br>
+                <span class="text-footer">Copyright &copy; Cosmetología Guadalajara</span>
+            </div>
+        </div>
     </footer>
 
     <script type="js/jquery.js"></script>
     <script type="js/bootstrap.min.js"></script>
     <script>
-	    $('.carousel').carousel({
-	        interval: 5000 
-	    })
+        $('.carousel').carousel({
+            interval: 5000 
+        })
     </script>
 
 </body>
