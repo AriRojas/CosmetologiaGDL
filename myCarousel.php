31bb01342c6455631338abb413a96d1f67419d3c
AriRojas <a7xariadna@gmail.com>
2016-04-19 22:18:01 -0500

    Agregando el código del slider en un archivo separado, ya que se utiliza en varias páginas.

---
+++ myCarousel.php
@@ -0,0 +1,47 @@
+<!-- Div Carousel -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="carousel-caption">
+                    <h2>Tratamientos Faciales</h2>      <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                </div>
+                <figure>                                <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                    <img src="images/chica_flor.jpg" alt="Tratamientos Faciales">
+                </figure>
+            </div>
+            <div class="item">
+                <div class="carousel-caption">
+                    <h2>Tratamientos Corporales</h2>    <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                </div>
+                <figure>                                <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                    <img src="images/Limpieza Facial.jpg" alt="Tratamientos Corporales">
+                <figure>
+            </div>
+            <div class="item">
+                <div class="carousel-caption">
+                    <h2>Promociones</h2>                <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                </div>
+                <figure>                                <!--Elemento a cambiar dependiendo de en que pagina se encuentre, con js-->
+                    <img src="images/Masaje.jpg" alt="Promociones">    
+                </figure>
+                
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </div>
