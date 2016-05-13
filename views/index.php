2d00b5eb03f14d02f6d5265000f16f878b3ae552
AriRojas <a7xariadna@gmail.com>
2016-05-12 23:47:46 -0500

    Creando MVC con carpetas y todo

---
+++ views/index.php
@@ -0,0 +1,152 @@
+    <?php include_once("header.php"); ?>
+	
+    <?php include_once("myCarousel.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Cuidados en tu piel
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Conócenos</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="nosotros.php" class="btn btn-default">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Conoce nuestras promociones</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="promociones.php" class="btn btn-default">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Haz una cita</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="contacto.php" class="btn btn-default">Ver más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Tratamientos -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Tratamientos de Faciales & Corporales</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 1</figcaption>
+                <figure>
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 2</figcaption>
+                <figure>
+                </a>
+            </div><div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 3</figcaption>
+                <figure>
+                </a>
+            </div><div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 4</figcaption>
+                <figure>
+                </a>
+            </div><div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 5</figcaption>
+                <figure>
+                </a>
+            </div><div class="col-md-4 col-sm-6">
+				<a href="#">
+				<figure class="galeria">                                
+                   <img class="img-responsive img-portfolio img-hover" src="images/Obsidiana700.jpg" alt=""/>
+					<figcaption>Descripcion imagen 6</figcaption>
+                <figure>
+                </a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Cosmetología Guadalajara</h2>
+            </div>
+            <div class="col-md-6">
+                <p>Registrate y participa en nuestras promociones:</p>
+                <ul>
+                    <li><strong>Ubicación</strong>
+                    </li>
+                    <li>Calle Zacatecas Norte #118</li>
+                    <li>Unión de Tula, Jalisco 48000</li>
+                    <li>Tel. (316) 371-0000</li>
+                    <li>Correo: contacto@cosmetologiagdl.com </li>
+                    <li>Lunes - Sábado: 9:00 AM a 5:00 PM</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="images/piel750.jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>No pierdar más tiempo!!   AGENDA una cita con nosotros HOY mismo y recibe un descuento del <strong>25%</strong>en cualquiera de los tratamientos que tenemos para TI.</p>
+                </div> 
+                <div class="col-md-4">
+                    <button id="btnAgendarCita" class="btn btn-lg btn-default btn-block">Agendar Cita</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include("footer.php"); ?>
+
+    <script>
+        $(document).ready( function(){
+            
+            $('.myCarousel').carousel({
+                interval: 2000; 
+            });
+
+        });
+    </script>
+
+</body>
+</html>
