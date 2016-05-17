b5de9adc9bcf4cf6f5dbcdb4bd47feb6f1e7ac07
AriRojas <a7xariadna@gmail.com>
2016-05-16 22:41:17 -0500

    Agregando controlador de Tratamientos y modificaciones a los archivos de vistas de los mismos

---
+++ controllers/TratamientosCtrl.php
@@ -0,0 +1,96 @@
+<?php
+    
+    class Tratamiento
+    {
+        
+        function __construct()
+        {
+            # code...
+        }
+
+        function cargarTratamiento()
+        {
+            if(isset($_GET['id']))
+            {
+                $header = file_get_contents('views/header.html');
+                $footer = file_get_contents('views/footer.html');
+                
+                switch ($_GET['id']) {
+                    case '1':
+                        $tratamiento = file_get_contents('views/Tratamientos-Faciales.html');
+                        break;
+                    
+                    case '2':
+                        $tratamiento = file_get_contents('views/Tratamientos-Corporales.html');
+                        break;
+
+                    case '1-1':
+                        # Limpieza Facial
+                        $tratamiento = file_get_contents('views/trat-LimpiezaFacial.html');
+                        break;
+
+                    case '1-2':
+                        # Antiacné
+                        $tratamiento = file_get_contents('views/trat-Antiacne.html');
+                        break;
+
+                    case '1-3':
+                        # Control de grasa
+                        $tratamiento = file_get_contents('views/trat-ControlDeGrasa.html');
+                        break;
+
+                    case '1-4':
+                        # Antiarrugas
+                        $tratamiento = file_get_contents('views/trat-Antiarrugas.html');
+                        break;
+
+                    case '1-5':
+                        # Manchas y cicatrices
+                        $tratamiento = file_get_contents('views/trat-ManchasCicatrices.html');
+                        break;
+
+                    case '1-6':
+                        # Desensibilizante
+                        $tratamiento = file_get_contents('views/trat-Desensibilizante.html');
+                        break;
+
+                    case '2-1':
+                        # Remodelado de silueta
+                        $tratamiento = file_get_contents('views/trat-RemodeladoSilueta.html');
+                        break;
+
+                    case '2-2':
+                        # Anticelulitis
+                        $tratamiento = file_get_contents('views/trat-Anticelulitis.html');
+                        break;
+
+                    case '2-3':
+                        # Antiestrias
+                        $tratamiento = file_get_contents('views/trat-Antiestrias.html');
+                        break;
+
+                    case '2-4':
+                        # Reafirmante de senos
+                        $tratamiento = file_get_contents('views/trat-ReafirmanteSenos.html');
+                        break;
+
+                    case '2-5':
+                        # Reductivo y reafirmante
+                        $tratamiento = file_get_contents('views/trat-ReductivoReafirmante.html');
+                        break;
+
+                    default:
+                        # code...
+                        break;
+                }
+
+                $view = $header . $tratamiento . $footer;
+                echo $view;
+            }
+
+        }
+    }
+
+
+
+?>
--- index.php
+++ index.php
@@ -3,25 +3,36 @@
 
     if(isset($_GET['ctl']))
     {
-        //echo "Siii";
         switch ($_GET['ctl']) {
             case 'nosotros':
                 require_once ('controllers/NosotrosCtrl.php');
                 $controller = new Nosotros();
+                $controller->cargarNosotros();
                 break;
 
             case 'contacto':
                 require_once 'controllers/ContactoCtrl.php';
                 $controller = new Contacto();
+                $controller->cargarContacto();
+                break;
+
+            case 'promociones':
+                require_once 'controllers/PromocionesCtrl.php';
+                $controller = new Promociones();
+                $controller->cargarPromociones();
+                break;
+
+            case 'tratamientos':
+                require_once 'controllers/TratamientosCtrl.php';
+                $controller = new Tratamiento();
+                $controller->cargarTratamiento();
                 break;
             
             default:
-                # code...
+                http_response_code(404);
                 break;
         }
 
-        
-        //$controller = new $url;
         
     }
     else
