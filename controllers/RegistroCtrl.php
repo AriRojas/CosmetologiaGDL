7e82629616588c69b7772e5197f5b1f129da2d16
AriRojas <a7xariadna@gmail.com>
2016-05-23 23:07:30 -0500

    Arreglando errores ¬¬

--- controllers/RegistroCtrl.php
+++ controllers/RegistroCtrl.php
@@ -19,23 +19,27 @@
             $this->model = new RegistroUsuario();
             $this->model->Ejecutar();
 
-            cargarRegistro();
-            //if(empty($_POST)){}
-            $nombre             = $_POST["nombre"];
-            $apellidoP          = $_POST["apellidoP"];
-            $apellidoM          = $_POST["apellidoM"];
-            $sexo               = $_POST["sexo"];
-            $direccion          = $_POST["direccion"];
-            $numExt             = $_POST["numExt"];
-            $numInt             = $_POST["numInt"];
-            $localidad          = $_POST["localidad"];
-            $municipio          = $_POST["municipio"];
-            $estado             = $_POST["estado"];
-            $telefono           = $_POST["telefono"];
-            $fechaNacimiento    = $_POST["fechaNacimiento"];
-            $mail               = $_POST["mail"];
-            $imagenPerfil       = $_POST["imagenPerfil"];
-            $password           = $_POST["password"];
+            if(empty($_POST)){
+                $this->cargarRegistro();
+
+            }
+            else{
+                $nombre             = $_POST["nombre"];
+                $apellidoP          = $_POST["apellidoP"];
+                $apellidoM          = $_POST["apellidoM"];
+                $sexo               = $_POST["sexo"];
+                $direccion          = $_POST["direccion"];
+                $numExt             = $_POST["numExt"];
+                $numInt             = $_POST["numInt"];
+                $localidad          = $_POST["localidad"];
+                $municipio          = $_POST["municipio"];
+                $estado             = $_POST["estado"];
+                $telefono           = $_POST["telefono"];
+                $fechaNacimiento    = $_POST["fechaNacimiento"];
+                $mail               = $_POST["mail"];
+                $imagenPerfil       = $_POST["imagenPerfil"];
+                $password           = $_POST["password"];
+            }
 
         }
 
