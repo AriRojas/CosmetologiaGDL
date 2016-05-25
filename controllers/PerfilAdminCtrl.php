39272e2eeb6764a3b09c0dfb7adc0c19aaa85e40
AriRojas <a7xariadna@gmail.com>
2016-05-25 01:19:20 -0500

    Modificación del perfil administrador. Mostrando barra de navegación con opciones funcionables. Falta modificar usuariosAdmin.html y el calendario de Citas

---
+++ controllers/PerfilAdminCtrl.php
@@ -0,0 +1,46 @@
+<?php
+    /**
+    * 
+    */
+    class PerfilAdmin
+    {
+        
+        function __construct()
+        {
+            # code...
+        }
+
+        public function cargarPerfilAdmin()
+        {
+            $header = file_get_contents('views/header.html');
+            $navAdmin = file_get_contents('views/navbarPerfilAdmin.html');
+
+            switch ($_GET['nav']) {
+                case 'principal':
+                    $content = file_get_contents('views/formDatosUsuario.html');
+                    break;
+
+                case 'usuarios':
+                    $content = file_get_contents('views/usuariosAdmin.html');
+                    break;
+
+                case 'citasAdmin':
+                    $content = file_get_contents('views/citasAdministrador.html');
+                    break;
+
+                case 'calendarioAdmin':
+                    $content = file_get_contents('views/calendarioCitas.html');
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+
+            $navAdmin = str_replace ( '{{CONTENT}}' ,  $content  ,  $navAdmin);
+
+            echo $header . $navAdmin;
+
+        }
+    }
+?>
