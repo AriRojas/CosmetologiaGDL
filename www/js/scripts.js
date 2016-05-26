57e57b54facda620fbff47a36e4ed57b40da368f
AriRojas <a7xariadna@gmail.com>
2016-05-26 00:12:48 -0500

    Modificación estructural de la vista del administrador, se borraron archivos innecesarios. Usaremos datetimepicker de bootstrap para el control de las citass, faltan modificaciones.

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,5 +1,23 @@
-function fnEditarPerilUsuario(elemento){	
-	foreach(i in elemento){
-		print(i);
-	}
-}
+
+$(function () {
+    $('#datetimepicker12').datetimepicker({
+        inline: true,
+        sideBySide: true
+    });
+});
+/*$("#datetimepicker12").datetimepicker({
+    inline: true,
+    sideBySide: true
+    isRTL: false,
+    format: 'dd.mm.yyyy hh:ii',
+    autoclose:true,
+    language: 'es'
+});
+*/
+$.fn.datetimepicker.dates['es'] = {
+    days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+    daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"],
+    daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
+    months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
+};
