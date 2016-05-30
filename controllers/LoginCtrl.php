1346de031f6215ac27d3ddbed236f64d73b92027
AriRojas <a7xariadna@gmail.com>
2016-05-29 23:11:32 -0500

    administración de usuarios

--- controllers/LoginCtrl.php
+++ controllers/LoginCtrl.php
@@ -16,27 +16,25 @@
             $header = file_get_contents('views/header.html');
             if (isset($_SESSION['usuario'])) {
 
-                
-                $navUser = '<a href="?ctl=miPerfil" class="btn btn-default"  aria-haspopup="true" aria-expanded="false">'
-                              . $_SESSION['usuario'] .
-                             '</a>' ;   
-                $header = str_replace('{PERFIL/REGISTRO}', $navUser , $header);
+                /*$contenido = "<a href=\"?ctl=registro\" class=\"btn btn-default\"  aria-haspopup=\"true\"aria-expanded=\"false\">"       .
+                    "        {PERFIL/REGISTRO}"     . 
+                    "</a>"                          .
 
-                $navLog = '<a href="?ctl=entrar&logout=true " class="btn btn-default"  aria-haspopup="true" aria-expanded="false">
-                            Salir 
-                           </a>';
-                $header = str_replace('{INGRESAR/SALIR}', $navLog, $header);
+                    "<a href=\"?ctl=entrar\" class=\"btn btn-default\"  aria-haspopup=\"true\" aria-expanded=\"false\">"                   .
+                    "        {INGRESAR/SALIR}"      . 
+                    "</a>";*/
+                //UNA VEZ LOGGEADO DEBO CAMBIAR EL HREF DE LOS DOS BOTONES DEL HEADER
+                //$i = strpos($header, '{BEGNOLOGGED}');
+                //$f = strpos($header, '{ENDNOLOGGED}');
+                //$contenido = substr($header, $i)
+                //$contenido = substr($header, start)
+                $header = str_replace('{PERFIL/REGISTRO}', $_SESSION['usuario'], $header);
+                $header = str_replace('{INGRESAR/SALIR}', 'Salir', $header);
             }
             else
             {
-                $navUser = ' <a href="?ctl=registro" class="btn btn-default"  aria-haspopup="true" aria-expanded="false">
-                                Registrarme
-                                </a>';
-                $header = str_replace('{PERFIL/REGISTRO}', $navUser, $header);
-                $navLog = '<a href="?ctl=entrar " class="btn btn-default"  aria-haspopup="true" aria-expanded="false">
-                            Ingresar 
-                           </a>';
-                $header = str_replace('{INGRESAR/SALIR}', $navLog, $header);
+                $header = str_replace('{PERFIL/REGISTRO}', 'Registrarme', $header);
+                $header = str_replace('{INGRESAR/SALIR}', 'Ingresar', $header);
             }
 
             return $header;
@@ -44,12 +42,6 @@
 
         public function cargarLogin()
         {
-            if( isset($_GET['logout']) &&  $_GET['logout']  == "true" ){
-                session_unset();
-                session_destroy();      
-                setcookie(session_name(), '', time()-3600);
-            }
-
             $header = $this->cargarHeader();
             $content = file_get_contents('views/entrar.html');
             $footer = file_get_contents('views/footer.html');
@@ -85,26 +77,116 @@
 
             if (empty($_POST)) {
                 $this->cargarLogin();
-                echo 'holoi';
             }
             else
             {
                 $correo  = $_POST["email"];
                 $pass    = $_POST["pass"];
-                echo 'hola';
+
+                //var_dump($correo);
+                //var_dump($_POST);
+                echo "<br/>" . $correo . "  " . $pass;
+
+                //header("Location: ?ctl=entrar&login=false");
                 $resultado = $this->model->Entrar($correo, $pass);
 
                 if($resultado == 1)
                 {
                     $_SESSION['usuario'] = $nombre . " " . $apellidoP;
-                    echo 'entra';
-                   // header('Location: ?ctl=miPerfil&login=true');
+                    header('Location: ?ctl=miPerfil&login=true');
                 
                 }elseif ( $resultado == -1) {
-                   echo 'no entra';
                     header('Location: ?ctl=entrar&login=false');     
                 }
             }
+        }
+
+
+        public function cargarPerfilAdmin()
+        {
+            require_once('controllers/MostrarDatosCtrl.php');
+            $this->model = new AdminDataGetter();
+
+            //$header = file_get_contents('views/header.html');
+            $header = $this->cargarHeader();
+            $navAdmin = file_get_contents('views/navbarPerfilAdmin.html');
+
+            if (isset($_SESSION['usuario'])) {
+                $navAdmin = str_replace('{{USERNAME}}', $_SESSION['usuario'], $navAdmin);
+            }
+
+            switch ($_GET['nav']) {
+                case 'principal':
+                    $content = file_get_contents('views/formDatosUsuario.html');
+                    break;
+
+                case 'usuarios':
+                    $content = file_get_contents('views/usuariosAdmin.html');
+                    //$this->model->todoUsuario();
+                    break;
+
+                case 'citasAdmin':
+                    $content = file_get_contents('views/citasAdministrador.html');
+                    break;
+
+                case 'calendarioAdmin':
+                    $content = file_get_contents('views/calendarioCitas.html');
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+
+            $navAdmin = str_replace ( '{{CONTENT}}' ,  $content  ,  $navAdmin);
+            $footer = file_get_contents('views/footer.html');
+            echo $header . $navAdmin . $footer;
+
+        }
+
+        public function cargarPerfilUsuario()
+        {
+            require_once('controllers/MostrarDatosCtrl.php');
+            $this->model = new UserDataGetter();
+
+            //$header = file_get_contents('views/header.html');
+            require_once('controllers/LoginCtrl.php');
+            $login = new Login();
+
+            $header = $login->cargarHeader();
+            $perfilUsuario = file_get_contents('views/perfilUsuario.html');
+            $footer = file_get_contents('views/footer.html');
+
+            if (isset($_SESSION['usuario'])) {
+                $perfilUsuario = str_replace('{{USERNAME}}', $_SESSION['usuario'], $perfilUsuario);
+            }
+
+            switch ($_GET['nav']) {
+                case 'principal':
+                    $content = file_get_contents('views/formDatosUsuario.html');
+                    break;
+
+                case 'historial':
+                    $content = file_get_contents('views/tablaHistorialUsuario.html');
+                    break;
+
+                case 'citasUsuario':
+                    $content = file_get_contents('views/citasAdministrador.html');
+                    break;
+
+                case 'calendarioUsuario':
+                    $content = file_get_contents('views/calendarioCitas.html');
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+
+            $perfilUsuario = str_replace('{{CONTENT}}', $content, $perfilUsuario);
+
+            $view = $header . $perfilUsuario . $footer;
+            echo $view;
         }
     }
 
--- controllers/MostrarDatosCtrl.php
+++ controllers/MostrarDatosCtrl.php
@@ -10,7 +10,7 @@
             # code...
         }
 
-        public function obtenerUsuarios()
+        public function todosUsuario()
         {
             if (empty($_POST)) {
                 
